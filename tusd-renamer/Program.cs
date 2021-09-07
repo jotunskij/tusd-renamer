8e5b17640c7790358ee4187179f33e558f44878e
Jens Tinglev <agitura@gmail.com>
2021-09-07 18:12:15 +0200

    Initial commit - working

--- tusd-renamer/Program.cs
+++ tusd-renamer/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -7,26 +6,47 @@ namespace tusd_renamer
 {
     public class TusdEntry
     {
+        [JsonProperty(PropertyName = "Upload")]
         public Upload Upload { get; set; }
     }
 
     public class Upload
     {
-        public string ID { get; set; }
-        public Dictionary<string, string> MetaData { get; set; }
+        [JsonProperty(PropertyName = "MetaData")]
+        public MetaData MetaData { get; set; }
+        [JsonProperty(PropertyName = "Storage")]
         public Storage Storage { get; set; }
     }
 
     public class Storage
     {
+        [JsonProperty(PropertyName = "Path")]
         public string Path { get; set; }
+    }
+
+    public class MetaData
+    {
+        [JsonProperty(PropertyName = "filename")]
+        public string filename { get; set; }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var stdin = Console.ReadLine();
+            string s;
+            string stdin = "";
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            while ((s = Console.ReadLine()) != null)
+            {
+                // Remove line comments
+                if (s.Trim().StartsWith("//"))
+                    continue;
+                // Trim utf-8 bom
+                stdin += s.Trim('\uFEFF', '\u200B');
+            }
+
             TusdEntry entry;
             try
             {
@@ -44,17 +64,11 @@ namespace tusd_renamer
                 return;
             }
 
-            if (!entry.Upload.MetaData.ContainsKey("filename"))
-            {
-                Console.WriteLine("Json metadata does not contain filename");
-                return;
-            }
-
             try
             {
                 var fileInfo = new FileInfo(entry.Upload.Storage.Path);
                 var directory = fileInfo.Directory.FullName;
-                var originalName = entry.Upload.MetaData["filename"];
+                var originalName = entry.Upload.MetaData.filename;
                 fileInfo.MoveTo(Path.Join(directory, originalName));
             }
             catch (Exception ex)
